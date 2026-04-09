8cbbbe321d62672f1fd6c259a71deaea16877837
kartikey <98679116+Kartikey-Tiwari@users.noreply.github.com>
2026-04-09 23:15:42 +0530

    refactored to add state machine, add body parsing

--- main.c
+++ main.c
@@ -30,10 +30,21 @@ void printRequestLine(RequestLine *rql) {
   printf("HTTP Version: %s\n", rql->http);
 }
 
+typedef enum RequestState {
+  INITIALIZED,
+  READ_REQUEST_LINE,
+  READ_HEADERS,
+  READING_BODY,
+  DONE
+} RequestState;
+
 typedef struct Request {
   RequestLine rql;
   GHashTable *headers;
-  int state;
+  char *body;
+  int contentLen;
+  int bytesRead;
+  RequestState state;
 } Request;
 
 void print_upper(gpointer key, gpointer value, gpointer user_data) {
@@ -51,6 +62,10 @@ void printRequest(Request *req) {
 
   printf("----Headers----\n");
   g_hash_table_foreach(req->headers, print_upper, NULL);
+
+  if (req->contentLen != 0) {
+    printf("----Body----\n%s\n----end----\n", req->body);
+  }
 }
 
 char isValidMethod(const char *method, char **methodsList) {
@@ -77,10 +92,16 @@ typedef enum {
   READ_NOT_PARSED,
   REQUEST_METHOD_ERROR,
   REQUEST_HTTP_VER_ERROR,
-  HEADER_FORMAT_ERROR
+  REQUEST_INVALID_CHAR_ERROR,
+  HEADER_FORMAT_ERROR,
+  PREPARED_BODY_PARSER,
+  READ_BODY,
+  BODY_LENGTH_MISMATCH,
+  SUCCESS_READ_BODY,
+  NOTHING_TO_DO
 } ErrorCode;
 
-ErrorCode readRequestLine(GString *line, RequestLine *rql) {
+ErrorCode readRequestLine(GString *line, Request *req) {
   ErrorCode rv = SUCCESS_READ_REQ_LINE;
   char **parts = g_strsplit(line->str, " ", -1);
   int i;
@@ -89,7 +110,7 @@ ErrorCode readRequestLine(GString *line, RequestLine *rql) {
   if (i == 3) {
     char *method = parts[0];
     if (!isValidMethod(method, httpMethods)) {
-      printf("bad request\n");
+      printf("request method error\n");
       rv = REQUEST_METHOD_ERROR;
       g_strfreev(parts);
       return rv;
@@ -98,7 +119,7 @@ ErrorCode readRequestLine(GString *line, RequestLine *rql) {
     char *version = parts[2];
 
     if (strcmp("HTTP/1.1", version) != 0) {
-      printf("bad request\n");
+      printf("http version error\n");
       rv = REQUEST_HTTP_VER_ERROR;
       g_strfreev(parts);
       return rv;
@@ -112,9 +133,9 @@ ErrorCode readRequestLine(GString *line, RequestLine *rql) {
         }
       }
 
-      rql->method = g_strdup(parts[0]);
-      rql->resource = g_strdup(parts[1]);
-      rql->http = g_strdup(version + j + 1);
+      req->rql.method = g_strdup(parts[0]);
+      req->rql.resource = g_strdup(parts[1]);
+      req->rql.http = g_strdup(version + j + 1);
     }
   }
   g_strfreev(parts);
@@ -147,7 +168,7 @@ int isValidHeaderValue(char *fieldValue) {
   return 1;
 }
 
-ErrorCode readHeaderLine(GString *line, GHashTable *headers) {
+ErrorCode readHeaderLine(GString *line, Request *req) {
   ErrorCode rv = SUCCESS_READ_HEADER;
   if (*(line->str) == '\0') {
     rv = SUCCESS_HEADERS_DONE;
@@ -176,14 +197,15 @@ ErrorCode readHeaderLine(GString *line, GHashTable *headers) {
       return rv;
     }
 
-    char *prevValue = g_hash_table_lookup(headers, field);
+    char *prevValue = g_hash_table_lookup(req->headers, field);
     if (prevValue == NULL) {
-      g_hash_table_insert(headers, g_strdup(field), g_strdup(value));
+      g_hash_table_insert(req->headers, g_strdup(field), g_strdup(value));
     } else {
       GString *newValue = g_string_new(prevValue);
       g_string_append(newValue, ",");
       g_string_append(newValue, value);
-      g_hash_table_insert(headers, g_strdup(field), g_strdup(newValue->str));
+      g_hash_table_insert(req->headers, g_strdup(field),
+                          g_strdup(newValue->str));
       g_string_free(newValue, TRUE);
     }
   } else {
@@ -193,64 +215,139 @@ ErrorCode readHeaderLine(GString *line, GHashTable *headers) {
   return rv;
 }
 
-ErrorCode parse(char *buf, int *bufSize, int *lookForStartingLF, GString *line,
-                Request *req) {
+ErrorCode readTillCRLF(char *buf, int *bufSize, int *lookForStartingLF,
+                       GString *line, Request *req,
+                       ErrorCode (*fn)(GString *, Request *)) {
   char *crlf = strstr(buf, "\r\n");
-  if (crlf != NULL) {
-    *crlf = '\0';
-  }
+  /* if (crlf != NULL) { */
+  /*   *crlf = '\0'; */
+  /* } */
 
   int rv;
 
   // last read end character was '\r' and this read first char is '\n'
-  if (*lookForStartingLF == 1 && buf[0] == '\n') {
-    if (req->state == 0) {
-      rv = readRequestLine(line, &req->rql);
-    } else if (req->state == 1) {
-      rv = readHeaderLine(line, req->headers);
+  if (*lookForStartingLF == 1) {
+    if (buf[0] == '\n') {
+      rv = fn(line, req);
+      line = g_string_truncate(line, 0);
+      memmove(buf, buf + 1, *bufSize - 1);
+      buf[*bufSize - 1] = '\0';
+      *bufSize = *bufSize - 1;
+      *lookForStartingLF = 0;
+    } else {
+      if (req->state == INITIALIZED) {
+        rv = REQUEST_INVALID_CHAR_ERROR;
+      } else if (req->state == READ_REQUEST_LINE) {
+        rv = HEADER_FORMAT_ERROR;
+      }
     }
-    line = g_string_truncate(line, 0);
-    memmove(buf, buf + 1, *bufSize - 1);
-    buf[*bufSize - 1] = '\0';
-    if (rv == SUCCESS_READ_REQ_LINE) {
-      req->state = 1;
-    } else if (rv == SUCCESS_HEADERS_DONE) {
-      req->state = 2;
-    }
-    *lookForStartingLF = 0;
     return rv;
   }
   // delimiter crlf not found
   else if (crlf == NULL) {
     if (buf[*bufSize - 1] == '\r') {
+      buf[*bufSize - 1] = '\0';
       *lookForStartingLF = 1;
     } else {
       *lookForStartingLF = 0;
     }
     g_string_append(line, buf);
+    *bufSize = 0;
     rv = READ_NOT_PARSED;
     return rv;
   }
   // crlf found
   else {
     *lookForStartingLF = 0;
+    *crlf = '\0';
     g_string_append(line, buf);
-    if (req->state == 0) {
-      rv = readRequestLine(line, &req->rql);
-    } else if (req->state == 1) {
-      rv = readHeaderLine(line, req->headers);
-    }
+    *crlf = '\r';
+    rv = fn(line, req);
     line = g_string_truncate(line, 0);
     *bufSize = *bufSize - (crlf - buf + 2);
     memmove(buf, crlf + 2, *bufSize);
     buf[*bufSize] = '\0';
-    if (rv == SUCCESS_READ_REQ_LINE) {
-      req->state = 1;
-    } else if (rv == SUCCESS_HEADERS_DONE) {
-      req->state = 2;
-    }
     return rv;
   }
+}
+
+ErrorCode readBodyWithLen(char *buf, int *bufSize, GString *line,
+                          Request *req) {
+  int bytesLeft = req->contentLen - req->bytesRead;
+
+  if (bytesLeft - *bufSize < 0) {
+    return BODY_LENGTH_MISMATCH;
+  }
+
+  g_string_append(line, buf);
+  req->bytesRead += *bufSize;
+  bytesLeft -= *bufSize;
+  *bufSize = 0;
+
+  if (!bytesLeft) {
+    req->body = g_strdup(line->str);
+    return SUCCESS_READ_BODY;
+  }
+
+  return READ_BODY;
+}
+
+int isValidContentLength(char *val) {
+  for (char *c = val; *c; c++) {
+    if (!isdigit(*c)) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+ErrorCode parse(char *buf, int *bufSize, int *lookForStartingLF, GString *line,
+                Request *req) {
+  int rv;
+  switch (req->state) {
+  case INITIALIZED:
+    rv = readTillCRLF(buf, bufSize, lookForStartingLF, line, req,
+                      readRequestLine);
+    if (rv == SUCCESS_READ_REQ_LINE) {
+      req->state = READ_REQUEST_LINE;
+    }
+    break;
+  case READ_REQUEST_LINE:
+    rv = readTillCRLF(buf, bufSize, lookForStartingLF, line, req,
+                      readHeaderLine);
+    if (rv == SUCCESS_HEADERS_DONE) {
+      req->state = READ_HEADERS;
+    }
+    break;
+  case READ_HEADERS:
+    char *value = g_hash_table_lookup(req->headers, "content-length");
+    if (value == NULL) {
+      req->state = DONE;
+      rv = NOTHING_TO_DO;
+      break;
+    }
+    if (isValidContentLength(value)) {
+      int len = atoi(value);
+      req->contentLen = len;
+      req->bytesRead = 0;
+      req->state = READING_BODY;
+      rv = PREPARED_BODY_PARSER;
+    } else {
+      rv = HEADER_FORMAT_ERROR;
+    }
+    break;
+  case READING_BODY:
+    rv = readBodyWithLen(buf, bufSize, line, req);
+    if (rv == SUCCESS_READ_BODY) {
+      req->state = DONE;
+    }
+    break;
+  case DONE:
+    rv = NOTHING_TO_DO;
+    break;
+  default:
+  }
+  return rv;
 }
 
 int main() {
@@ -266,6 +363,8 @@ int main() {
     perror("socket: couldn't open");
     exit(1);
   }
+  int opt = 1;
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
   if (bind(sockfd, (struct sockaddr *)&address, sizeof address) < 0) {
     perror("bind: failed");
     exit(1);
@@ -298,27 +397,36 @@ int main() {
       char buf[9];
       buf[8] = '\0';
 
-      int rv, lookForStartingLF = 0;
+      int bytesReceived, lookForStartingLF = 0;
       GString *line = g_string_new(NULL);
       Request req = {0};
-      req.state = 0;
+      req.state = INITIALIZED;
       req.headers =
           g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
 
-      while ((rv = recv(new_fd, buf, 8, 0)) > 0) {
+      while ((bytesReceived = recv(new_fd, buf, 8, 0)) >= 0) {
+        int rv = bytesReceived;
         buf[rv] = '\0';
 
-        int k = parse(buf, &rv, &lookForStartingLF, line, &req);
-        while (k != READ_NOT_PARSED && k != SUCCESS_HEADERS_DONE) {
+        ErrorCode k = parse(buf, &rv, &lookForStartingLF, line, &req);
+        while (rv != 0) {
           k = parse(buf, &rv, &lookForStartingLF, line, &req);
         }
-        if (k == SUCCESS_HEADERS_DONE) {
+        if (k == SUCCESS_READ_BODY || bytesReceived == 0 ||
+            k == NOTHING_TO_DO || req.state == DONE) {
           break;
         }
       }
-      printRequest(&req);
+      if (req.state == DONE) {
+        printRequest(&req);
+      }
       g_string_free(line, TRUE);
+      g_free(req.body);
       g_hash_table_destroy(req.headers);
+      req.headers = NULL;
+      g_free(req.rql.method);
+      g_free(req.rql.resource);
+      g_free(req.rql.http);
       close(new_fd);
       exit(0);
     }
